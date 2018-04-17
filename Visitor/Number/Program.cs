e8dc683e0f5abfa1790436a20ec4488e39744c4c
Fedor Kravchuk <fedor@mail.ru>
2018-04-17 16:36:50 +0300

    Finish

---
+++ Visitor/Number/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[] { 7, 8, 3, 2, 5 };
+            ConsoleKey key;
+            int cur = num.Length - 1;
+            int pos = num.Length - 1;       
+            do
+            {
+                foreach (var item in num)
+                    Console.Write(item);
+                Console.CursorLeft = pos;
+                key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.LeftArrow:
+                        if (cur == 0)
+                            cur = pos = num.Length;
+                        cur--;
+                        pos--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        cur++;
+                        pos++;
+                        if (cur == num.Length)
+                            cur = pos = 0;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        num[cur]++;
+                        if (num[cur] == 10)
+                            num[cur] = 0;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        num[cur]--;
+                        if (num[cur] == -1)
+                            num[cur] = 9;
+                        break;
+                }
+                Console.Clear();
+            } while(key != ConsoleKey.Escape);
+          
+        }
+    }
+}
---
+++ Visitor/Visitor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visitor
+{
+    public static class XXX
+    {
+        public static void HelloWorld(this String a)
+        {
+            Console.WriteLine(a);
+        }
+        //public static int First(this int[] a)
+        //{
+        //    return a[0];
+        //}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string s = "vova";
+            //s.HelloWorld();
+            int[] arr = new int[] { 8, 4, 7, 3, 1 };
+            Console.WriteLine(arr.First());
+        }
+    }
+}
